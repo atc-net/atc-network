1ae73de091bae02d2708d7d0f599c2b6f24b379b
Per Kops <pko@delegate.dk>
2022-08-01 11:35:17 +0200

    feat: Initial solution structure

---
+++ src/Atc.Network/GlobalUsings.cs
@@ -0,0 +1,14 @@
+global using System.ComponentModel.DataAnnotations;
+global using System.Diagnostics.CodeAnalysis;
+global using System.Net;
+global using System.Net.Http.Headers;
+global using System.Net.Mime;
+global using System.Text;
+global using System.Text.Json;
+global using System.Text.Json.Serialization;
+
+global using Atc.Data.Models;
+global using Atc.Helpers;
+global using Atc.Serialization;
+
+global using Mapster;
