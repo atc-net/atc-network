3e740c423466ea3a5780c21b481b48837967c324
David Kallesen <david@lmdk.dk>
2022-08-11 19:09:26 +0200

    fix: Fix usings and add to resharper-spell-dictonary

--- src/Atc.Network/GlobalUsings.cs
+++ src/Atc.Network/GlobalUsings.cs
@@ -1,8 +1,17 @@
+global using System.Collections.Concurrent;
+global using System.ComponentModel.DataAnnotations;
+global using System.Diagnostics;
 global using System.Diagnostics.CodeAnalysis;
 global using System.Net;
+global using System.Net.NetworkInformation;
 global using System.Net.Sockets;
 global using System.Text;
+global using System.Text.RegularExpressions;
 
+global using Atc.Helpers;
+global using Atc.Network.Helpers;
+global using Atc.Network.Internet;
+global using Atc.Network.Models;
 global using Atc.Network.Tcp;
 
 global using Microsoft.Extensions.Logging;
--- test/Atc.Network.Test/GlobalUsings.cs
+++ test/Atc.Network.Test/GlobalUsings.cs
@@ -1,10 +1,15 @@
+global using System.Diagnostics;
 global using System.Diagnostics.CodeAnalysis;
+global using System.Net;
 global using System.Reflection;
 global using System.Text;
 
 global using Atc.CodeDocumentation.Markdown;
+global using Atc.Network.Helpers;
+global using Atc.Network.Internet;
 global using Atc.Network.Tcp;
 global using Atc.XUnit;
 
 global using Microsoft.Extensions.Logging.Abstractions;
+
 global using Xunit.Abstractions;
