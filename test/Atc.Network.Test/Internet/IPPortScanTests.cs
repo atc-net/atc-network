2c240466705a1b22346f1ac01313dbe58c4a45dc
David Kallesen <david@lmdk.dk>
2022-08-28 21:40:11 +0200

    feat: Add some test for IPPortScan

---
+++ test/Atc.Network.Test/Internet/IPPortScanTests.cs
@@ -0,0 +1,82 @@
+namespace Atc.Network.Test.Internet;
+
+[SuppressMessage("Reliability", "CA2000:Dispose objects before losing scope", Justification = "OK.")]
+[Trait(Traits.Category, Traits.Categories.Integration)]
+[Trait(Traits.Category, Traits.Categories.SkipWhenLiveUnitTesting)]
+public class IPPortScanTests
+{
+    [Fact]
+    public async Task CanConnectWithTcp()
+    {
+        // Arrange
+        var testIpAddress = GetTestIpAddress();
+
+        var ipPortScan = new IPPortScan(testIpAddress);
+
+        // Act
+        var actual = await ipPortScan.CanConnectWithTcp(
+            80,
+            CancellationToken.None);
+
+        // Assert
+        Assert.False(actual);
+    }
+
+    [Fact]
+    public async Task CanConnectWithHttp()
+    {
+        // Arrange
+        var testIpAddress = GetTestIpAddress();
+
+        var ipPortScan = new IPPortScan(testIpAddress);
+
+        // Act
+        var actual = await ipPortScan.CanConnectWithHttp(
+                80,
+                CancellationToken.None);
+
+        // Assert
+        Assert.False(actual);
+    }
+
+    [Fact]
+    public async Task CanConnectWithHttps()
+    {
+        // Arrange
+        var testIpAddress = GetTestIpAddress();
+
+        var ipPortScan = new IPPortScan(testIpAddress);
+
+        // Act
+        var actual = await ipPortScan.CanConnectWithHttps(
+            80,
+            CancellationToken.None);
+
+        // Assert
+        Assert.False(actual);
+    }
+
+    [Fact]
+    public async Task CanConnectWithHttpOrHttps()
+    {
+        // Arrange
+        var testIpAddress = GetTestIpAddress();
+
+        var ipPortScan = new IPPortScan(testIpAddress);
+
+        // Act
+        var actual = await ipPortScan.CanConnectWithHttpOrHttps(
+            80,
+            true,
+            CancellationToken.None);
+
+        // Assert
+        Assert.False(actual);
+    }
+
+    private static IPAddress GetTestIpAddress()
+    {
+        var localAddress = IPv4AddressHelper.GetLocalAddress();
+        return localAddress ?? IPAddress.Parse("8.8.8.8");
+    }
+}
