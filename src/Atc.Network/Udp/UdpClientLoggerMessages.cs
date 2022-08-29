512dae0b463bb399365d81ee27bf527709999716
David Kallesen <david@lmdk.dk>
2022-08-29 10:24:41 +0200

    feat: Init UpdClient

---
+++ src/Atc.Network/Udp/UdpClientLoggerMessages.cs
@@ -0,0 +1,11 @@
+namespace Atc.Network.Udp;
+
+/// <summary>
+/// UdpClient LoggerMessages.
+/// </summary>
+[SuppressMessage("Design", "MA0048:File name must match type name", Justification = "OK - By Design")]
+[SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "OK")]
+public partial class UdpClient
+{
+    private readonly ILogger logger;
+}
