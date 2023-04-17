c027a95135b42bc7d80ba2b0434fa314dc9a8c74
David Kallesen <david@lmdk.dk>
2023-04-17 16:43:16 +0200

    fix: Spelling in file name & class name

--- src/Atc.Network/Udp/UdpConstants.cs
+++ src/Atc.Network/Udp/UdpConstants.cs
@@ -4,8 +4,13 @@ namespace Atc.Network.Udp;
 /// This class contains default constant for
 /// <see cref="UdpClient"/> and <see cref="UdpServer"/>.
 /// </summary>
-public static class UpdConstants
+public static class UdpConstants
 {
+    /// <summary>
+    /// The send/receive time-out value, in milliseconds.
+    /// </summary>
+    public const int DefaultSendReceiveTimeout = 0;
+
     /// <summary>
     /// The send/receive buffer value, in bytes. The default is 8192 (8 Kb);
     /// </summary>
